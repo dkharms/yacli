ba145c8037e34bd6b053cbe8d9c16dbd4265e59c
Daniel Kharms <dkharmsd@gmail.com>
2023-04-04 20:15:56 +0300

    feat(yacli): implemented parser

--- parser.go
+++ parser.go
@@ -5,36 +5,69 @@ import (
 	"strings"
 )
 
+// repository is a struct that represents a collection of arguments passed to CLI tool.
+// It contains the positional arguments and flags provided by the user.
 type repository struct {
-	// Positional argument.
-	posarg []string
-	// Flags.
-	mf map[string]string
+	// A slice of strings representing the arguments passed before the flags.
+	// These are the arguments that do not start with a dash (-) or double-dash (--).
+	beforeFlags []string
+
+	// A map that contains the flags and their values.
+	// The keys represent the flag names, and the values represent their values.
+	// Flags are the arguments that start with a dash (-) or double-dash (--).
+	flagSet map[string]string
+
+	// A slice of strings representing the positional arguments.
+	// These are the arguments that are not flags, and their order matters.
+	// They are provided after the beforeFlags and the flags.
+	positionalArgs []string
 }
 
+// parser represents a command-line argument parser
+// which holds the list of unparsed arguments and a boolean
+// flag indicating whether the flags have been parsed or not.
 type parser struct {
-	fm   int
-	args []string
+	// osargs is a slice of strings representing the command-line arguments
+	// to be parsed.
+	osargs []string
+
+	// parsedFlags is a boolean value indicating whether or not the flags
+	// have been parsed yet. If this value is true, then all remaining command-line
+	// arguments are treated as positional arguments and are not parsed as flags.
+	parsedFlags bool
 }
 
 func newParser(args []string) *parser {
-	return &parser{args: args}
+	return &parser{osargs: args}
 }
 
+// parse is a method of the parser type which parses the
+// command-line arguments and returns a repository and error.
+// The repository contains the parsed flags and positional
+// arguments, while the error will be non-nil if there was an error
+// encountered during parsing.
 func (p *parser) parse() (repository, error) {
-	var posargs []string
+	beforeFlags, _ := p.parseTillFlags()
 
-	r := repository{mf: make(map[string]string)}
+	r := repository{
+		beforeFlags:    beforeFlags,
+		flagSet:        make(map[string]string),
+		positionalArgs: []string{},
+	}
 
 	var i int
-	for i < len(p.args) {
-		arg := p.args[i]
+	for i < len(p.osargs) {
+		arg := p.osargs[i]
 
 		switch {
 		case isLongFlag(arg):
+			if p.parsedFlags {
+				return repository{}, fmt.Errorf("already parsed flags: invalid argument '%s'", arg)
+			}
+
 			targ := strings.TrimPrefix(arg, "--")
 			if len(arg) == 0 {
-				return repository{}, fmt.Errorf("'%s' is not valid flag", arg)
+				return repository{}, fmt.Errorf("invalid flag: '%s'", arg)
 			}
 
 			var name, value string
@@ -47,27 +80,74 @@ func (p *parser) parse() (repository, error) {
 
 			if len(parts) == 2 {
 				value = parts[1]
-			} else if i+1 < len(p.args) && !(isShortFlag(p.args[i+1]) || isLongFlag(p.args[i+1])) {
-				value = p.args[i+1]
+			} else if i+1 < len(p.osargs) && !(isShortFlag(p.osargs[i+1]) || isLongFlag(p.osargs[i+1])) {
+				value = p.osargs[i+1]
 				i++
 			}
 
-			r.mf[name] = value
-			i++
+			r.flagSet[name] = value
 		case isShortFlag(arg):
+			if p.parsedFlags {
+				return repository{}, fmt.Errorf("already parsed flags: invalid argument '%s'", arg)
+			}
+
+			targ := strings.TrimPrefix(arg, "-")
+			if len(targ) == 0 {
+				return repository{}, fmt.Errorf("invalid flag: '%s'", arg)
+			}
+
+			if len(targ) > 1 {
+				for _, l := range targ {
+					r.flagSet[string(l)] = ""
+				}
+			} else if i+1 < len(p.osargs) && !(isShortFlag(p.osargs[i+1]) || isLongFlag(p.osargs[i+1])) {
+				r.flagSet[targ] = p.osargs[i+1]
+				i++
+			} else {
+				r.flagSet[targ] = ""
+			}
 		default:
-			posargs = append(posargs, arg)
-			i++
+			r.positionalArgs = append(r.positionalArgs, arg)
+			p.parsedFlags = true
 		}
+
+		i++
 	}
 
 	return r, nil
 }
 
+// parseTillFlags is a method of the parser struct
+// that parses the command line arguments passed to a GoLang program till it encounters a flag argument.
+// It returns a slice of strings containing the arguments before the flags
+// and a boolean value indicating whether it encountered a flag argument or not.
+func (p *parser) parseTillFlags() ([]string, bool) {
+	var beforeFlags []string
+
+	for len(p.osargs) > 0 {
+		arg := p.osargs[0]
+		if isShortFlag(arg) || isLongFlag(arg) {
+			return beforeFlags, true
+		}
+		p.osargs = p.osargs[1:]
+		beforeFlags = append(beforeFlags, arg)
+	}
+
+	return beforeFlags, false
+}
+
+// isLongFlag is a function that takes a single argument
+// of type string and returns a boolean value indicating
+// whether the given string is a long flag or not.
+// A long flag is a command-line argument that starts with a double dash (--).
 func isLongFlag(arg string) bool {
 	return strings.HasPrefix(arg, "--")
 }
 
+// isShortFlag is a function that takes a single argument
+// of type string and returns a boolean value indicating
+// whether the given string is a short flag or not.
+// A short flag is a command-line argument that starts with a single dash (-).
 func isShortFlag(arg string) bool {
 	return strings.HasPrefix(arg, "-")
 }
--- parser_test.go
+++ parser_test.go
@@ -16,42 +16,42 @@ func TestParser_long(t *testing.T) {
 			name:  "--key value",
 			flags: []string{"--key", "value"},
 			expected: repository{
-				mf: map[string]string{"key": "value"},
+				flagSet: map[string]string{"key": "value"},
 			},
 		},
 		{
 			name:  "--key=value",
 			flags: []string{"--key=value"},
 			expected: repository{
-				mf: map[string]string{"key": "value"},
+				flagSet: map[string]string{"key": "value"},
 			},
 		},
 		{
 			name:  "--key='value'",
 			flags: []string{"--key='value'"},
 			expected: repository{
-				mf: map[string]string{"key": "'value'"},
+				flagSet: map[string]string{"key": "'value'"},
 			},
 		},
 		{
 			name:  "--key=",
 			flags: []string{"--key="},
 			expected: repository{
-				mf: map[string]string{"key": ""},
+				flagSet: map[string]string{"key": ""},
 			},
 		},
 		{
 			name:  "--key ikey=ivalue",
 			flags: []string{"--key", "ikey=ivalue"},
 			expected: repository{
-				mf: map[string]string{"key": "ikey=ivalue"},
+				flagSet: map[string]string{"key": "ikey=ivalue"},
 			},
 		},
 		{
 			name:  "--key=ikey=ivalue",
 			flags: []string{"--key=ikey=ivalue"},
 			expected: repository{
-				mf: map[string]string{"key": "ikey=ivalue"},
+				flagSet: map[string]string{"key": "ikey=ivalue"},
 			},
 		},
 		{
@@ -68,7 +68,7 @@ func TestParser_long(t *testing.T) {
 			name:  "--akey --bkey bvalue",
 			flags: []string{"--akey", "--bkey", "value"},
 			expected: repository{
-				mf: map[string]string{"akey": "", "bkey": "value"},
+				flagSet: map[string]string{"akey": "", "bkey": "value"},
 			},
 		},
 	}
@@ -83,8 +83,135 @@ func TestParser_long(t *testing.T) {
 				t.Fatal(err)
 			}
 
-			if !reflect.DeepEqual(tt.expected.mf, r.mf) {
-				t.Fatalf("expected %v, got %v", tt.expected.mf, r.mf)
+			if !reflect.DeepEqual(tt.expected.flagSet, r.flagSet) {
+				t.Fatalf("expected %v, got %v", tt.expected.flagSet, r.flagSet)
+			}
+		})
+	}
+}
+
+func TestParser_short(t *testing.T) {
+	testCase := []struct {
+		name     string
+		flags    []string
+		expected repository
+		hasErr   bool
+	}{
+		{
+			name:  "-k value",
+			flags: []string{"-k", "value"},
+			expected: repository{
+				flagSet: map[string]string{"k": "value"},
+			},
+		},
+		{
+			name:  "-abc",
+			flags: []string{"-abc"},
+			expected: repository{
+				flagSet: map[string]string{"a": "", "b": "", "c": ""},
+			},
+		},
+		{
+			name:  "-a -b -c",
+			flags: []string{"-a", "-b", "-c"},
+			expected: repository{
+				flagSet: map[string]string{"a": "", "b": "", "c": ""},
+			},
+		},
+		{
+			name:  "-a -b -c value",
+			flags: []string{"-a", "-b", "-c", "value"},
+			expected: repository{
+				flagSet: map[string]string{"a": "", "b": "", "c": "value"},
+			},
+		},
+		{
+			name:  "-a value",
+			flags: []string{"-a", "-b", "-c", "value"},
+			expected: repository{
+				flagSet: map[string]string{"a": "", "b": "", "c": "value"},
+			},
+		},
+	}
+
+	for _, tt := range testCase {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			p := newParser(tt.flags)
+
+			r, err := p.parse()
+			if err != nil && !tt.hasErr {
+				t.Fatal(err)
+			}
+
+			if !reflect.DeepEqual(tt.expected.flagSet, r.flagSet) {
+				t.Fatalf("expected %v, got %v", tt.expected.flagSet, r.flagSet)
+			}
+		})
+	}
+}
+
+func TestParser_mixed(t *testing.T) {
+	testCase := []struct {
+		name     string
+		args     []string
+		expected repository
+		hasErr   bool
+	}{
+		{
+			name: "command subcommand argument",
+			args: []string{"command", "subcommand", "argument"},
+			expected: repository{
+				beforeFlags:    []string{"command", "subcommand", "argument"},
+				flagSet:        map[string]string{},
+				positionalArgs: []string{},
+			},
+		},
+		{
+			name: "command subcommand --flag flag-value argument",
+			args: []string{"command", "subcommand", "--flag", "flag-value", "argument"},
+			expected: repository{
+				beforeFlags:    []string{"command", "subcommand"},
+				flagSet:        map[string]string{"flag": "flag-value"},
+				positionalArgs: []string{"argument"},
+			},
+		},
+		{
+			name: "command subcommand --flag flag-value --another-flag=argument",
+			args: []string{"command", "subcommand", "--flag", "flag-value", "--another-flag=argument"},
+			expected: repository{
+				beforeFlags:    []string{"command", "subcommand"},
+				flagSet:        map[string]string{"flag": "flag-value", "another-flag": "argument"},
+				positionalArgs: []string{},
+			},
+		},
+		{
+			name:   "command subcommand --flag flag-value incorrect-argument --another-flag=argument",
+			args:   []string{"command", "subcommand", "--flag", "flag-value", "incorrect-argument", "--another-flag=argument"},
+			hasErr: true,
+		},
+	}
+
+	for _, tt := range testCase {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			p := newParser(tt.args)
+
+			r, err := p.parse()
+			if err != nil && !tt.hasErr {
+				t.Fatal(err)
+			}
+
+			if !reflect.DeepEqual(tt.expected.beforeFlags, r.beforeFlags) {
+				t.Fatalf("before flags: expected %v, got %v", tt.expected.beforeFlags, r.beforeFlags)
+			}
+
+			if !reflect.DeepEqual(tt.expected.flagSet, r.flagSet) {
+				t.Fatalf("flag set: expected %v, got %v", tt.expected.flagSet, r.flagSet)
+			}
+
+			if !reflect.DeepEqual(tt.expected.positionalArgs, r.positionalArgs) {
+				t.Fatalf("positional args: expected %v, got %v", tt.expected.positionalArgs, r.positionalArgs)
 			}
 		})
 	}
