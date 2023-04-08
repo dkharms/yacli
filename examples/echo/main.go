065dd1b854d64abf51a6541483c691aaf6b32ff2
Daniel Kharms <dkharmsd@gmail.com>
2023-04-08 10:57:28 +0300

    feat(yacli): added `echo` example

---
+++ examples/echo/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/dkharms/yacli"
+)
+
+var root = yacli.NewRootCommand(
+	yacli.WithCommandDescription("Just prints <message> in format you specified"),
+	yacli.WithMutualExclusiveFlags(
+		yacli.NewFlag("uppercase", "u", "Print <message> in uppercase", yacli.Bool),
+		yacli.NewFlag("lowercase", "l", "Print <message> in lowercase", yacli.Bool),
+	),
+	yacli.WithAguments(
+		yacli.NewArgument("message", "Message to print", yacli.String),
+		yacli.NewArgument("amount", "Print <message> `n` times", yacli.Integer),
+	),
+	yacli.WithAction(echo),
+)
+
+func echo(ctx yacli.Context) error {
+	n := int(ctx.Arguments().Integer("amount"))
+	message := ctx.Arguments().String("message")
+
+	if v, isSet := ctx.Flags().Bool("uppercase"); isSet && v {
+		message = strings.ToUpper(message)
+	}
+
+	if v, isSet := ctx.Flags().Bool("lowercase"); isSet && v {
+		message = strings.ToLower(message)
+	}
+
+	for i := 0; i < n; i++ {
+		fmt.Println(message)
+	}
+
+	return nil
+}
+
+func main() {
+	if err := root.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
