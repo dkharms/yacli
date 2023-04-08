e1d65fb505267344298dda43655c5d2997305e31
Daniel Kharms <dkharmsd@gmail.com>
2023-04-08 09:41:24 +0300

    docs(yacli): more comments in `argument`

--- argument.go
+++ argument.go
@@ -4,14 +4,22 @@ import "fmt"
 
 type argumentOption func(*argument)
 
+// Argument represents a command argument.
 type Argument interface {
+	// Name returns the name of the argument.
 	Name() string
+
+	// Value returns the value of the argument.
 	Value() any
+
+	// Type returns the type of the argument.
 	Type() ytype
 }
 
 var _ (Argument) = (*argument)(nil)
 
+// argument represents a command-line argument with a name, description,
+// type, value, and custom validators.
 type argument struct {
 	name        string
 	description string
@@ -20,7 +28,11 @@ type argument struct {
 	cvalidators []func(Argument) error
 }
 
-func NewArgument(name, description string, ttype ytype, opts ...argumentOption) *argument {
+// NewArgument creates a new argument with the given name, description,
+// and type, and applies the specified options to it.
+func NewArgument(
+	name, description string, ttype ytype, opts ...argumentOption,
+) *argument {
 	a := &argument{
 		name:        name,
 		description: description,
@@ -34,24 +46,32 @@ func NewArgument(name, description string, ttype ytype, opts ...argumentOption) 
 	return a
 }
 
+// WithArgumentValidator is an argumentOption function that allows adding custom validators to an argument.
+// It takes a function with an Argument parameter that returns an error, and appends it to the argument's
+// list of custom validators.
 func WithArgumentValidator(v func(Argument) error) argumentOption {
 	return func(a *argument) {
 		a.cvalidators = append(a.cvalidators, v)
 	}
 }
 
+// Name returns the name of the argument.
 func (a *argument) Name() string {
 	return a.name
 }
 
+// Type returns the type of the argument.
 func (a *argument) Type() ytype {
 	return a.ttype
 }
 
+// Value returns the value of the argument.
 func (a *argument) Value() any {
 	return a.value
 }
 
+// validate validates the argument value against its type and custom validators.
+// It returns an error if the validation fails.
 func (a *argument) validate() error {
 	if a.value == nil {
 		return fmt.Errorf("invalid argument: missing value for argument '%s'", a.name)
