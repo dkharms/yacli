d4d7376e4b3e2b4a76ae02fd6491e5185f2564d1
Daniel Kharms <dkharmsd@gmail.com>
2023-04-08 09:52:34 +0300

    test(yacli): implemented tests for `ytype`

---
+++ types_test.go
@@ -0,0 +1,107 @@
+package yacli
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestValidateInteger(t *testing.T) {
+	tests := []struct {
+		name    string
+		v       any
+		want    interface{}
+		wantErr bool
+	}{
+		{"valid positive int", "123", int(123), false},
+		{"valid negative int", "-123", int(-123), false},
+		{"invalid int", "123a", 0, true},
+		{"valid positive int8", "123", int8(123), false},
+		{"valid negative int8", "-123", int8(-123), false},
+		{"invalid int8", "123a", 0, true},
+		{"valid positive int16", "123", int16(123), false},
+		{"valid negative int16", "-123", int16(-123), false},
+		{"invalid int16", "123a", 0, true},
+		{"valid positive int32", "123", int32(123), false},
+		{"valid negative int32", "-123", int32(-123), false},
+		{"invalid int32", "123a", 0, true},
+		{"valid positive int64", "123", int64(123), false},
+		{"valid negative int64", "-123", int64(-123), false},
+		{"invalid int64", "123a", 0, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var (
+				got any
+				err error
+			)
+
+			switch reflect.TypeOf(tt.want).Kind() {
+			case reflect.Int:
+				got, err = validateInteger[int](tt.v)
+			case reflect.Int8:
+				got, err = validateInteger[int8](tt.v)
+			case reflect.Int16:
+				got, err = validateInteger[int16](tt.v)
+			case reflect.Int32:
+				got, err = validateInteger[int32](tt.v)
+			case reflect.Int64:
+				got, err = validateInteger[int64](tt.v)
+			}
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf(
+					"validateInteger('%v')=%v, error= %v, wantErr=%v",
+					tt.v, got, err, tt.wantErr,
+				)
+				return
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("validateInteger('%v')=%v, want=%v", tt.v, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestValidateFloat(t *testing.T) {
+	tests := []struct {
+		name    string
+		v       any
+		want    interface{}
+		wantErr bool
+	}{
+		{"valid positive float32", "123.45", float32(123.45), false},
+		{"valid negative float32", "-123.45", float32(-123.45), false},
+		{"invalid float32", "123.45o", float32(0), true},
+		{"valid positive float64", "123.45", float64(123.45), false},
+		{"valid negative float64", "-123.45", float64(-123.45), false},
+		{"invalid float64", "123.45o", float64(0), true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var (
+				got any
+				err error
+			)
+
+			switch reflect.TypeOf(tt.want).Kind() {
+			case reflect.Float32:
+				got, err = validateFloat[float32](tt.v)
+			case reflect.Float64:
+				got, err = validateFloat[float64](tt.v)
+			}
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf(
+					"validateFloat('%v')=%v error=%v, wantErr %v",
+					tt.v, got, err, tt.wantErr,
+				)
+				return
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("validateFloat('%v')=%v, want %v", tt.v, got, tt.want)
+			}
+		})
+	}
+}
