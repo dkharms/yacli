f9d64987a246d4804dd917769b840379f5e2b1fb
Daniel Kharms <dkharmsd@gmail.com>
2023-04-11 10:39:10 +0300

    feat(yacli): POSIX usage and `--help`

---
+++ help.go
@@ -0,0 +1,48 @@
+package yacli
+
+import (
+	"fmt"
+	"text/template"
+)
+
+var (
+	formatRed     = func(msg string) string { return fmt.Sprintf("\033[31m%s\033[0m", msg) }
+	formatBlue    = func(msg string) string { return fmt.Sprintf("\033[34m%s\033[0m", msg) }
+	formatDefault = func(msg string) string { return fmt.Sprintf("\033[0m%s\033[0m", msg) }
+)
+
+var (
+	formatBold = func(msg string) string { return fmt.Sprintf("\033[1m%s\033[0m", msg) }
+)
+
+var helpTemplateRaw = `{{ .Usage }}
+{{ .Description }}
+
+Flags:
+{{- range .Flags }}
+    {{ printf "-%s" .Short | FormatBold }} | {{printf "--%s" .Name | FormatBold }} [{{ printf "%s" .Type | FormatBlue }}] - {{ .Description }} 
+{{- end }}
+{{- if gt (len .Arguments) 0 }} 
+Arguments:
+{{- range .Arguments }}
+    {{ if not .Optional}}{{ FormatBold "*" | FormatRed }}{{end}} {{ FormatBold .Name }} [{{ printf "%s" .Type | FormatBlue }}] - {{ .Description }} 
+{{- end }}
+{{- end }}
+{{- if gt (len .Subcommands) 0 }}
+Subcommands:
+{{- range .Subcommands }}
+    {{ FormatBold .Name }} - {{ .Description }}
+{{- end }}
+{{- end }}
+`
+
+var helpTemplate = template.Must(
+	template.New("help").Funcs(
+		map[string]any{
+			"FormatDefault": formatDefault,
+			"FormatRed":     formatRed,
+			"FormatBlue":    formatBlue,
+			"FormatBold":    formatBold,
+		},
+	).Parse(helpTemplateRaw),
+)
---
+++ posix.go
@@ -0,0 +1,80 @@
+package yacli
+
+import (
+	"fmt"
+	"strings"
+)
+
+func (c *command) Usage() string {
+	var s strings.Builder
+
+	s.WriteString(c.Name())
+
+	var (
+		defaultGroup  []*flag
+		mutexGroup    []*flag
+		togetherGroup []*flag
+	)
+
+	for _, g := range c.fg {
+		switch g.ttype {
+		case groupDefault:
+			defaultGroup = append(defaultGroup, g.flags...)
+		case groupMutex:
+			mutexGroup = append(mutexGroup, g.flags...)
+		case groupTogether:
+			togetherGroup = append(togetherGroup, g.flags...)
+		}
+	}
+
+	if len(defaultGroup) > 0 {
+		s.WriteString(formatDefaultGroup(defaultGroup...))
+	}
+
+	if len(mutexGroup) > 0 {
+		s.WriteString(formatMutexGroup(mutexGroup...))
+	}
+
+	if len(togetherGroup) > 0 {
+		s.WriteString(formatTogetherGroup(togetherGroup...))
+	}
+
+	for _, arg := range c.as {
+		s.WriteString(fmt.Sprintf(" %s", arg.Name()))
+	}
+
+	return s.String()
+}
+
+func formatDefaultGroup(flags ...*flag) string {
+	var s strings.Builder
+	for _, f := range flags {
+		s.WriteString(" [")
+		s.WriteString(fmt.Sprintf(" -%s", f.Short()))
+		s.WriteString(" ]")
+	}
+	return s.String()
+}
+
+func formatMutexGroup(flags ...*flag) string {
+	var s strings.Builder
+	s.WriteString(" [")
+	for i, f := range flags {
+		s.WriteString(fmt.Sprintf(" -%s", f.Short()))
+		if i < len(flags)-1 {
+			s.WriteString(" |")
+		}
+	}
+	s.WriteString(" ]")
+	return s.String()
+}
+
+func formatTogetherGroup(flags ...*flag) string {
+	var s strings.Builder
+	s.WriteString(" [")
+	for _, f := range flags {
+		s.WriteString(fmt.Sprintf(" -%s", f.Short()))
+	}
+	s.WriteString(" ]")
+	return s.String()
+}
